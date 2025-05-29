10deb52430eecc9264b5ea20efcada8d10780e61
Leonardo Gross <leogrossms@gmail.com>
2025-05-29 11:00:49 -0300

    feat:  defined props for AButton component

---
+++ src/components/AButton.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+defineProps({
+  label: String,
+  icon: String,
+  iconClass: {
+    type: String,
+    default: ''
+  },
+  iconPos: {
+    type: String,
+    default: 'left',
+  },
+  loading: Boolean,
+  link: String,
+  severety: {
+    type: String,
+    default: 'Primary',
+  },
+  disabled: Boolean,
+  raised: Boolean,
+  rounded: Boolean,
+  text: Boolean,
+  raisedText: Boolean,
+  outlined: Boolean,
+  iconOnly: Boolean,
+  badge: [String, Number],
+  size: String,
+  template: String,
+  buttonGroup: Boolean
+})
+
+</script>
+
+<template>
+  <button></button>
+</template>
+
+<style scoped>
+
+</style>
