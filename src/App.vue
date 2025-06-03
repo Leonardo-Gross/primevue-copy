2ffc6acf1add2dad37f1125ab108357adff6b5df
Leonardo Gross <leogrossms@gmail.com>
2025-06-03 16:18:50 -0300

    feat :Continuation in recreating the options of the PrimeVue Button component.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
 import AButton from './components/AButton.vue';
+import { ref } from 'vue'
+
+
+const loading = ref(false)
+
+const load = async () => {
+  loading.value = true
+  await new Promise(resolve => setTimeout(resolve, 2000))
+  loading.value = false
+}
 
 </script>
 
@@ -11,12 +21,39 @@ import AButton from './components/AButton.vue';
         <AButton label="Submit"/>
       </div>
     </div>
+
     <div class="m-5">
       <h2 class="text-2xl font-bold mb-4">Icons</h2>
-      <div class="bg-gray-50 rounded-lg p-5 card flex justify-center flex-wrap gap-4 border border-gray-200 border-solid">
-        <AButton icon="pi pi-home" iconPos="right"/>
+      <div class="bg-gray-50 rounded-lg p-5 card  border border-gray-200 border-solid">
+        <div class="flex justify-center gap-4">
+          <AButton icon="pi pi-home"/>
+          <AButton label="Profile" icon="pi pi-user" iconPos="left"/>
+          <AButton label="Save" icon="pi pi-check" iconPos="right"/>
+        </div>
+        <div class="flex justify-center gap-4 mt-4">
+          <AButton label="Profile" icon="pi pi-search" iconPos="top"/>
+          <AButton label="Profile" icon="pi pi-refresh" iconPos="bottom"/>
+        </div>
+
       </div>
     </div>
+
+    <div class=" m-5">
+      <h2 class="text-2xl font-bold mb-4">Loading</h2>
+      <div class="flex justify-center bg-gray-50 rounded-lg p-5 card  border border-gray-200 border-solid">
+        <AButton label="Search" icon="pi pi-check" iconPos="right" :loading="loading" @click="load" />
+      </div>
+    </div>
+
+    <div class=" m-5">
+      <h2 class="text-2xl font-bold mb-4">Link</h2>
+      <div class="flex justify-center bg-gray-50 rounded-lg p-5 card  border border-gray-200 border-solid gap-4">
+        <AButton label="Link" link/>
+        <AButton as="a" label="External" href="https://vuejs.org/" target="_blank"/>
+        <AButton label="Router"/>
+      </div>
+    </div>
+
     <div class="m-5">
       <h2 class="text-2xl font-bold mb-4">Severity</h2>
         <div class="bg-gray-50 rounded-lg p-5 card flex justify-center flex-wrap gap-4 border border-gray-200 border-solid">
@@ -28,6 +65,139 @@ import AButton from './components/AButton.vue';
           <AButton label="Help" severity="help"/>
           <AButton label="Danger" severity="danger"/>
           <AButton label="Contrast" severity="contrast"/>
+        </div>
+    </div>
+
+    <div class=" m-5">
+      <h2 class="text-2xl font-bold mb-4">Disabled</h2>
+      <div class="flex justify-center bg-gray-50 rounded-lg p-5 card  border border-gray-200 border-solid">
+        <AButton label="Submit" disabled/>
+      </div>
+    </div>
+
+    <div class="m-5">
+      <h2 class="text-2xl font-bold mb-4">Raised</h2>
+        <div class="bg-gray-50 rounded-lg p-5 card flex justify-center flex-wrap gap-4 border border-gray-200 border-solid">
+          <AButton label="Primary" severity="primary" raised/>
+          <AButton label="Secondary" severity="secondary" raised/>
+          <AButton label="Success" severity="success" raised/>
+          <AButton label="Info" severity="info" raised/>
+          <AButton label="Warn" severity="warn" raised/>
+          <AButton label="Help" severity="help" raised/>
+          <AButton label="Danger" severity="danger" raised/>
+          <AButton label="Contrast" severity="contrast" raised/>
+        </div>
+    </div>
+
+    <div class="m-5">
+      <h2 class="text-2xl font-bold mb-4">Rounded</h2>
+        <div class="bg-gray-50 rounded-lg p-5 card flex justify-center flex-wrap gap-4 border border-gray-200 border-solid">
+          <AButton label="Primary" severity="primary" rounded/>
+          <AButton label="Secondary" severity="secondary" rounded/>
+          <AButton label="Success" severity="success" rounded/>
+          <AButton label="Info" severity="info" rounded/>
+          <AButton label="Warn" severity="warn" rounded/>
+          <AButton label="Help" severity="help" rounded/>
+          <AButton label="Danger" severity="danger" rounded/>
+          <AButton label="Contrast" severity="contrast" rounded/>
+        </div>
+    </div>
+
+    <div class="m-5">
+      <h2 class="text-2xl font-bold mb-4">Text</h2>
+        <div class="bg-gray-50 rounded-lg p-5 card flex justify-center flex-wrap gap-4 border border-gray-200 border-solid">
+          <AButton label="Primary" severity="primary" variant="text"/>
+          <AButton label="Secondary" severity="secondary" variant="text"/>
+          <AButton label="Success" severity="success" variant="text"/>
+          <AButton label="Info" severity="info" variant="text"/>
+          <AButton label="Warn" severity="warn" variant="text"/>
+          <AButton label="Help" severity="help" variant="text"/>
+          <AButton label="Danger" severity="danger" variant="text"/>
+          <AButton label="Contrast" severity="contrast" variant="text"/>
+        </div>
+    </div>
+
+     <div class="m-5">
+      <h2 class="text-2xl font-bold mb-4">Raised Text</h2>
+        <div class="bg-gray-50 rounded-lg p-5 card flex justify-center flex-wrap gap-4 border border-gray-200 border-solid">
+          <AButton label="Primary" severity="primary" raised variant="text"/>
+          <AButton label="Secondary" severity="secondary" raised variant="text"/>
+          <AButton label="Success" severity="success" raised variant="text"/>
+          <AButton label="Info" severity="info" raised variant="text"/>
+          <AButton label="Warn" severity="warn" raised variant="text"/>
+          <AButton label="Help" severity="help" raised variant="text"/>
+          <AButton label="Danger" severity="danger" raised variant="text"/>
+          <AButton label="Contrast" severity="contrast" raised variant="text"/>
+        </div>
+    </div>
+
+     <div class="m-5">
+      <h2 class="text-2xl font-bold mb-4">Outlined</h2>
+        <div class="bg-gray-50 rounded-lg p-5 card flex justify-center flex-wrap gap-4 border border-gray-200 border-solid">
+          <AButton label="Primary" severity="primary" variant="outlined"/>
+          <AButton label="Secondary" severity="secondary" variant="outlined"/>
+          <AButton label="Success" severity="success" variant="outlined"/>
+          <AButton label="Info" severity="info" variant="outlined"/>
+          <AButton label="Warn" severity="warn" variant="outlined"/>
+          <AButton label="Help" severity="help" variant="outlined"/>
+          <AButton label="Danger" severity="danger" variant="outlined"/>
+          <AButton label="Contrast" severity="contrast" variant="outlined"/>
+        </div>
+    </div>
+
+    <div class="m-5">
+      <h2 class="text-2xl font-bold mb-4">Icon Only</h2>
+        <div class=" bg-gray-50 rounded-lg p-5 justify-center gap-4 border border-gray-200 border-solid">
+          <div class="flex justify-center gap-4 mb-6">
+            <AButton icon="pi pi-check" iconOnly severity="primary"/>
+            <AButton icon="pi pi-bookmark" iconOnly severity="secondary"/>
+            <AButton icon="pi pi-search" iconOnly severity="success"/>
+            <AButton icon="pi pi-user" iconOnly severity="info"/>
+            <AButton icon="pi pi-bell" iconOnly severity="warn"/>
+            <AButton icon="pi pi-heart" iconOnly severity="help"/>
+            <AButton icon="pi pi-times" iconOnly severity="danger"/>
+            <AButton icon="pi pi-star" iconOnly severity="contrast"/>
+          </div>
+          <div class="flex justify-center gap-4 mb-6">
+            <AButton icon="pi pi-check" iconOnly severity="primary" rounded/>
+            <AButton icon="pi pi-bookmark" iconOnly severity="secondary" rounded/>
+            <AButton icon="pi pi-search" iconOnly severity="success" rounded/>
+            <AButton icon="pi pi-user" iconOnly severity="info" rounded/>
+            <AButton icon="pi pi-bell" iconOnly severity="warn" rounded/>
+            <AButton icon="pi pi-heart" iconOnly severity="help" rounded/>
+            <AButton icon="pi pi-times" iconOnly severity="danger" rounded/>
+            <AButton icon="pi pi-star" iconOnly severity="contrast" rounded/>
+          </div>
+          <div class="flex justify-center gap-4 mb-6">
+            <AButton icon="pi pi-check" iconOnly severity="primary" rounded variant="outlined"/>
+            <AButton icon="pi pi-bookmark" iconOnly severity="secondary" rounded variant="outlined"/>
+            <AButton icon="pi pi-search" iconOnly severity="success" rounded variant="outlined"/>
+            <AButton icon="pi pi-user" iconOnly severity="info" rounded variant="outlined"/>
+            <AButton icon="pi pi-bell" iconOnly severity="warn" rounded variant="outlined"/>
+            <AButton icon="pi pi-heart" iconOnly severity="help" rounded variant="outlined"/>
+            <AButton icon="pi pi-times" iconOnly severity="danger" rounded variant="outlined"/>
+            <AButton icon="pi pi-star" iconOnly severity="contrast" rounded variant="outlined"/>
+          </div>
+          <div class="flex justify-center gap-4 mb-6">
+            <AButton icon="pi pi-check" iconOnly severity="primary" rounded raised variant="text"/>
+            <AButton icon="pi pi-bookmark" iconOnly severity="secondary" rounded raised variant="text"/>
+            <AButton icon="pi pi-search" iconOnly severity="success" rounded raised variant="text"/>
+            <AButton icon="pi pi-user" iconOnly severity="info" rounded raised variant="text"/>
+            <AButton icon="pi pi-bell" iconOnly severity="warn" rounded raised variant="text"/>
+            <AButton icon="pi pi-heart" iconOnly severity="help" rounded raised variant="text"/>
+            <AButton icon="pi pi-times" iconOnly severity="danger" rounded raised variant="text"/>
+            <AButton icon="pi pi-star" iconOnly severity="contrast" rounded raised variant="text"/>
+          </div>
+          <div class="flex justify-center gap-4 mb-6">
+            <AButton icon="pi pi-check" iconOnly severity="primary" rounded variant="text"/>
+            <AButton icon="pi pi-bookmark" iconOnly severity="secondary" rounded variant="text"/>
+            <AButton icon="pi pi-search" iconOnly severity="success" rounded variant="text"/>
+            <AButton icon="pi pi-user" iconOnly severity="info" rounded variant="text"/>
+            <AButton icon="pi pi-bell" iconOnly severity="warn" rounded variant="text"/>
+            <AButton icon="pi pi-heart" iconOnly severity="help" rounded variant="text"/>
+            <AButton icon="pi pi-times" iconOnly severity="danger" rounded variant="text"/>
+            <AButton icon="pi pi-star" iconOnly severity="contrast" rounded variant="text"/>
+          </div>
         </div>
     </div>
   </div>
--- src/components/AButton.vue
+++ src/components/AButton.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-import { computed, h } from 'vue'
-
-type Severity = 'primary' | 'secondary' | 'success' | 'info' | 'warn' | 'help' | 'danger' | 'contrast'
+import { ref, computed, h, type PropType } from 'vue'
 
 const props = defineProps({
   label: String,
@@ -12,80 +10,157 @@ const props = defineProps({
   },
   iconPos: {
     type: String,
-    default: 'left',
+    default: 'left'
   },
-  loading: Boolean,
-  link: String,
-  severity: {
-    type: String,
-    default: 'primary',
+  link: {
+    type: Boolean,
+    default: false
   },
+  href: String,
+  target: String,
   disabled: Boolean,
   raised: Boolean,
   rounded: Boolean,
-  text: Boolean,
-  outlined: Boolean,
+  variant: {
+    type: String as PropType<'text' | 'outlined' | 'contained'>,
+    default: 'contained'
+  },
   iconOnly: Boolean,
   badge: [String, Number],
   size: String,
-  template: String,
-  buttonGroup: Boolean
+  severity: {
+    type: String,
+    default: 'primary'
+  },
+  autoLoading: {
+    type: Boolean,
+    default: false
+  }
 })
 
-const severityStyles: Record<Severity, string> = {
-  primary: 'bg-gray-950 hover:bg-gray-800 text-white',
-  secondary: 'bg-gray-300 hover:bg-gray-400 text-gray-600',
-  success: 'bg-green-500 hover:bg-green-600 text-white',
-  info: 'bg-blue-400 hover:bg-blue-500 text-white',
-  warn: 'bg-orange-400 hover:bg-orange-500 text-white',
-  help: 'bg-purple-600 hover:bg-purple-700 text-white',
-  danger: 'bg-red-500 hover:bg-red-600 text-white',
-  contrast: 'bg-gray-950 hover:bg-gray-800 text-white'
+const modelLoading = defineModel<boolean>('loading')
+const emit = defineEmits(['click'])
+
+const isLoadingInternally = ref(false)
+
+const isLoading = computed(() => {
+  return props.autoLoading
+    ? isLoadingInternally.value
+    : modelLoading?.value ?? false
+})
+
+const handleClick = async (e: Event) => {
+  if (props.disabled || isLoading.value) return
+
+  emit('click', e)
+
+  if (props.autoLoading && modelLoading) {
+    modelLoading.value = true
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    modelLoading.value = false
+  }
 }
 
-const buttonClasses = computed(() => {
-  const severityKey = (props.severity?.toLowerCase() ?? 'primary') as Severity
-  const severityClass = severityStyles[severityKey]
+type Severity =
+  | 'primary' | 'secondary' | 'success' | 'info'
+  | 'warn' | 'help' | 'danger' | 'contrast'
 
+const severityKey = (props.severity?.toLowerCase() ?? 'primary') as Severity
+
+const severityVariants = computed(() => {
+  if (props.variant === 'text') {
+    return {
+      primary: 'text-gray-950 hover:bg-gray-100',
+      secondary: 'text-gray-600 hover:bg-gray-100',
+      success: 'text-green-500 hover:bg-green-50',
+      info: 'text-blue-500 hover:bg-blue-50',
+      warn: 'text-orange-500 hover:bg-orange-50',
+      help: 'text-purple-600 hover:bg-purple-50',
+      danger: 'text-red-500 hover:bg-red-50',
+      contrast: 'text-gray-950 hover:bg-gray-100'
+    }[severityKey]
+  }
+
+  if (props.variant === 'outlined') {
+    return {
+      primary: 'border border-gray-200 text-gray-950 hover:bg-gray-100',
+      secondary: 'border border-gray-300 text-gray-600 hover:bg-gray-100',
+      success: 'border border-green-500 text-green-600 hover:bg-green-50',
+      info: 'border border-blue-400 text-blue-600 hover:bg-blue-50',
+      warn: 'border border-orange-400 text-orange-600 hover:bg-orange-50',
+      help: 'border border-purple-600 text-purple-700 hover:bg-purple-50',
+      danger: 'border border-red-500 text-red-600 hover:bg-red-50',
+      contrast: 'border border-gray-950 text-gray-950 hover:bg-gray-100'
+    }[severityKey]
+  }
+
+  // contained (default)
   return {
-    // Base
+    primary: 'bg-gray-950 hover:bg-gray-800 text-white',
+    secondary: 'bg-gray-300 hover:bg-gray-400 text-gray-600',
+    success: 'bg-green-500 hover:bg-green-600 text-white',
+    info: 'bg-blue-400 hover:bg-blue-500 text-white',
+    warn: 'bg-orange-400 hover:bg-orange-500 text-white',
+    help: 'bg-purple-600 hover:bg-purple-700 text-white',
+    danger: 'bg-red-500 hover:bg-red-600 text-white',
+    contrast: 'bg-gray-950 hover:bg-gray-800 text-white'
+  }[severityKey]
+})
+
+const layoutClass = computed(() => {
+  return ['top', 'bottom'].includes(props.iconPos) ? 'flex-col gap-1' : 'flex-row gap-2'
+})
+
+const buttonClasses = computed(() => {
+  return {
     'inline-flex items-center justify-center transition-colors duration-200 cursor-pointer rounded': true,
-
-    // Severity
-    [severityClass]: true,
-
-    // State
-    'opacity-50 cursor-not-allowed': props.disabled,
-    'shadow-md': props.raised,
+    [layoutClass.value]: true,
+    [severityVariants.value]: !props.link,
+    'opacity-50 !cursor-not-allowed': props.disabled || isLoading.value,
+    '!shadow-md/30': props.raised,
     'rounded-full': props.rounded,
-    'bg-transparent text-gray-700 hover:bg-gray-100': props.text,
-    'border border-gray-400 bg-transparent text-gray-700': props.outlined,
-    'p-2': props.iconOnly && !props.label,
+    'w-10 h-10': props.iconOnly && !props.label,
     'p-1 px-3': !(props.iconOnly && !props.label),
-
-    // Size
     'text-sm px-2 py-1': props.size === 'small',
     'text-lg px-4 py-2': props.size === 'large',
+    'bg-transparent text-black font-medium hover:underline hover:bg-white': props.link
   }
 })
 
 const iconVNode = computed(() => {
+  if (isLoading.value) {
+    return h('span', {
+      class: 'pi pi-spinner animate-spin text-base'
+    })
+  }
+
   if (!props.icon) return null
-  const spacing = props.iconPos === 'left' ? 'mr-2' : 'ml-2'
+
   return h('i', {
-    class: `${props.icon} ${props.iconClass} ${props.label ? spacing : ''}`
+    class: `${props.icon} ${props.iconClass}`
   })
 })
 
 const labelVNode = computed(() => {
   return props.label ? h('span', {}, props.label) : null
 })
+
+const iconFirst = computed(() => {
+  return props.iconPos === 'left' || props.iconPos === 'top'
+})
 </script>
 
 <template>
-  <button :class="buttonClasses" :disabled="props.disabled">
-    <component :is="iconVNode" v-if="props.icon && props.iconPos === 'left'" />
+  <component
+    :class="buttonClasses"
+    :is="props.href ? 'a' : 'button'"
+    :href="props.href"
+    :target="props.target"
+    :disabled="props.disabled || isLoading"
+    @click="handleClick"
+  >
+    <component :is="iconVNode" v-if="(props.icon || isLoading) && iconFirst" />
     <component :is="labelVNode" v-if="labelVNode" />
-    <component :is="iconVNode" v-if="props.icon && props.iconPos === 'right'" />
-  </button>
+    <component :is="iconVNode" v-if="(props.icon || isLoading) && !iconFirst" />
+  </component>
 </template>
