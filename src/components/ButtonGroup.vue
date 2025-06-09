b5b9eae43719dc38a8c084612f68b5955977c6ea
Leonardo Gross <leogrossms@gmail.com>
2025-06-09 10:11:51 -0300

    feat: add dynamic sizes, badges, button group, and custom template to buttons

---
+++ src/components/ButtonGroup.vue
@@ -0,0 +1,10 @@
+ <script setup lang="ts">
+import { provide } from 'vue';
+
+provide('inButtonGroup', true)
+</script>
+<template>
+  <div class="inline-flex rounded-lg overflow-hidden shadow-sm !border-0">
+    <slot/>
+  </div>
+</template>
