88ca00f4aaa467054ee35edeed233c36f873b906
Leonardo Gross <leogrossms@gmail.com>
2025-06-18 13:57:19 -0300

    feat: implemented ALayout in ACardview

--- src/views/ACardView.vue
+++ src/views/ACardView.vue
@@ -1,31 +1,12 @@
 <script setup lang="ts">
 import AButton from '@/components/AButton.vue';
 import ACard from '@/components/ACard.vue';
-import { ref } from 'vue';
+import ALayout from '@/components/ALayout.vue';
 
-const isSideBarOpen = ref(false)
-function toggleSideBar() {
-  isSideBarOpen.value = !isSideBarOpen.value
-}
 </script>
 <template>
   <div class="w-full h-full p-10 bg-gray-100">
-    <div>
-      <div class="flex justify-between items-center">
-        <h1 class="text-3xl font-bold">Card Component</h1>
-        <i @click="toggleSideBar" class="pi pi-plus"></i>
-      <div v-if="isSideBarOpen" class="p-2 bg-gray-400 text-white rounded-l-lg fixed top-0 right-0 h-full w-64 !p-4 shadow-lg">
-        <div class="flex justify-between items-baseline">
-          <h2 class="text-2xl font-bold mb-4">Components</h2>
-          <i @click="toggleSideBar" class="pi pi-times"></i>
-        </div>
-        <ul class="mt-10 ms-5">
-          <li class="mb-5"><RouterLink to="/">Button</RouterLink></li>
-          <li><RouterLink to="/card">Card</RouterLink></li>
-        </ul>
-      </div>
-    </div>
-    </div>
+    <ALayout title="Card Component"/>
     <div class="m-5">
       <h2 class="text-2xl font-bold mb-4">Basic</h2>
       <ACard>
