6854a8147c57d34301cf6aa122faba80f5a9fbd3
Leonardo Gross <leogrossms@gmail.com>
2025-06-18 13:56:12 -0300

    feat: implement ADivider component and ADividerView (not finished)

---
+++ src/components/ADivider.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps<{
+  type?: 'solid' | 'dotted' | 'dashed',
+  vertical?: boolean,
+  align?: 'left' | 'right' | 'center',
+}>()
+
+const lineClasses = computed(() => {
+  return props.vertical
+  ? 'w-px h-full border-border-gray-300'
+  : 'w-px w-full border-border-gray-300'
+} )
+</script>
+
+<template>
+  abuble
+</template>
