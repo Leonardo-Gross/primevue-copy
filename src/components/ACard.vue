b174dd759b86b4c36fda9ea66107d7a556d34c88
Guilherme Battoni <guilherme.battoni@gmail.com>
2025-06-18 12:48:27 -0300

    refactor(ACard): update to handle slots.

--- src/components/ACard.vue
+++ src/components/ACard.vue
@@ -1,23 +1,62 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, useSlots } from 'vue';
+
+const slots = useSlots();
+
+const showDefaultSlot = computed(
+  () =>
+    slots?.default &&
+    !slots?.header &&
+    !slots?.title &&
+    !slots?.subtitle &&
+    !slots?.content &&
+    !slots?.footer
+);
+
+const showContentWrapper = computed(
+  () => slots?.title || slots?.subtitle || slots?.content || slots?.footer
+);
+</script>
 
 <template>
   <div
-    class="w-full flex-wrap justify-start rounded-lg border border-solid border-gray-200 bg-gray-50 !p-0 p-5 text-slate-800"
+    class="w-full flex-wrap justify-start rounded-lg border border-solid border-gray-200 bg-gray-50 p-0 text-slate-800"
   >
-    <div class="w-full text-gray-700">
+    <slot v-if="showDefaultSlot" />
+
+    <div
+      v-if="$slots?.header"
+      class="w-full text-gray-700"
+    >
       <slot name="header" />
     </div>
-    <div class="p-5">
-      <div class="mt-4 flex text-lg font-medium">
+
+    <div
+      v-if="showContentWrapper"
+      class="p-5"
+    >
+      <div
+        v-if="$slots?.title"
+        class="mt-4 flex text-lg font-medium"
+      >
         <slot name="title" />
       </div>
-      <div class="text-md mt-4 flex font-normal text-gray-400">
+
+      <div
+        v-if="$slots?.subtitle"
+        class="text-md mt-4 flex font-normal text-gray-400"
+      >
         <slot name="subtitle" />
       </div>
-      <div class="mt-1 text-gray-700">
+
+      <div
+        v-if="$slots?.content"
+        class="mt-1 text-gray-700"
+      >
         <slot name="content" />
       </div>
-      <div>
+
+      <div v-if="$slots?.footer">
         <slot name="footer" />
       </div>
     </div>
