4a5d9815d676e44d23ce02eeb96b9c8c5f58ba69
Leonardo Gross <leogrossms@gmail.com>
2025-07-11 09:10:05 -0300

    feat(Enum): implemented new enum INPUTTYPE  and put into convention

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,3 +1,39 @@
+export enum ALIGN {
+  LEFT = 'left',
+  CENTER = 'center',
+  RIGHT = 'right',
+}
+
+export enum INPUTTYPE {
+  BUTTON = 'button',
+  CHECKBOX = 'checkbox',
+  COLOR = 'color',
+  DATE = 'date',
+  DATETIME_LOCAL = 'datetime-local',
+  EMAIL = 'email',
+  FILE = 'file',
+  HIDDEN = 'hidden',
+  IMAGE = 'image',
+  MONTH = 'month',
+  NUMBER = 'number',
+  PASSWORD = 'password',
+  RADIO = 'radio',
+  RANGE = 'range',
+  RESET = 'reset',
+  SEARCH = 'search',
+  SUBMIT = 'submit',
+  TEL = 'tel',
+  TEXT = 'text',
+  TIME = 'time',
+  URL = 'url',
+  WEEK = 'week',
+}
+
+export enum ORIENTATION {
+  HORIZONTAL = 'horizotal',
+  VERTICAL = 'vertical',
+}
+
 export enum POSITION {
   BOTTOM = 'bottom',
   LEFT = 'left',
@@ -22,11 +58,15 @@ export enum SIZE {
   SMALL = 'small',
 }
 
+export enum STYLETYPE {
+  SOLID = 'solid',
+  DOTTED = 'dotted',
+  DASHED = 'dashed',
+}
+
 export enum VARIANT {
   CONTAINED = 'contained',
   OUTLINED = 'outlined',
   TEXT = 'text',
+  FILLED = 'filled',
 }
-
-
-
