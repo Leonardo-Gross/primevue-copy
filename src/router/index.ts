733a2c29a7412cbb0e4a1374f632dfe55749c04a
Leonardo Gross <leogrossms@gmail.com>
2025-07-24 11:25:43 -0300

    feat(router): create new route for ATextAreaView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import AButtonView from '../views/AButtonView.vue';
 import ACardView from '../views/ACardView.vue';
+import ADividerView from '../views/ADividerView.vue'
+import AInputMaskView from '../views/AInputMaskView.vue';
+import AInputTextView from '../views/AInputTextView.vue';
 import APanelView from '../views/APanelView.vue';
- import ADividerView from '../views/ADividerView.vue'
+import ATextAreaView from '../views/ATextAreaView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,11 +25,26 @@ const router = createRouter({
       name: 'panel',
       component: APanelView,
     },
-     {
-       path: '/divider',
-       name: 'divider',
-       component: ADividerView,
-     }
+    {
+      path: '/divider',
+      name: 'divider',
+      component: ADividerView,
+    },
+    {
+      path: '/inputtext',
+      name: 'inputtext',
+      component: AInputTextView,
+    },
+    {
+      path: '/inputmask',
+      name: 'inputmask',
+      component: AInputMaskView,
+    },
+    {
+      path: '/textarea',
+      name: 'textarea',
+      component: ATextAreaView,
+    }
   ],
 });
 
